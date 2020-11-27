754736953eefb1456b61c6040a8d073bbf8c4e79
Vikram Kriplaney <vikram.kriplaney@netcetera.com>
2020-11-27 10:08:25 +0100

    Initial Commit

---
+++ SwiftAlpsWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftAlpsWeather
+//
+//  Created by Vikram Kriplaney on 27.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftAlpsWeather/SwiftAlpsWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAlpsWeatherApp.swift
+//  SwiftAlpsWeather
+//
+//  Created by Vikram Kriplaney on 27.11.20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAlpsWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
