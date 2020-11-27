42ba12cd8ecafbc154e5e53527e17b8a5a2fcb18
Vikram Kriplaney <vikram.kriplaney@netcetera.com>
2020-11-27 15:59:41 +0100

    Step 6: Add location model, style weather symbols, etc.

--- SwiftAlpsWeather/ContentView.swift
+++ SwiftAlpsWeather/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 import Combine
 
-struct ContentView: View {
-    @StateObject var viewModel = ViewModel()
+struct LoadingOverlay: View {
+    let isLoading: Bool
 
     var body: some View {
-        List(viewModel.cities) { city in
-            Text(city.name)
-        }.onAppear {
-            viewModel.fetch()
+        if isLoading {
+            Text("Loading...")
+                .foregroundColor(.white)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 5)
+                        .foregroundColor(.black)
+                        .opacity(0.5)
+                )
+                .transition(.scale).animation(.spring())
+        }
+    }
+}
+
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+//    @StateObject var locationModel = LocationModel()
+
+    var body: some View {
+        NavigationView {
+            List(viewModel.cities) { city in
+                ZStack {
+                    Circle()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.secondary)
+                    city.weather.first?.image
+                        .renderingMode(.original)
+                        .imageScale(.large)
+                        .shadow(radius: 2)
+                }
+
+                Text(city.name).bold()
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("\(city.main.temp, specifier: "%.1f")°C")
+                    HStack {
+                        Text("\(city.main.tempMin, specifier: "%.1f")°C")
+                            .foregroundColor(.blue)
+                        Text("\(city.main.tempMax, specifier: "%.1f")°C")
+                            .foregroundColor(.red)
+                    }.font(.caption)
+                }
+            }
+            .overlay(
+                LoadingOverlay(isLoading: viewModel.isLoading)
+            )
+            .alert(isPresented: $viewModel.hasError, content: {
+                Alert(title: Text(viewModel.error?.localizedDescription ?? "Error"))
+            })
+            .onAppear {
+                viewModel.fetch()
+            }.navigationTitle("Swift Alps Weather")
         }
     }
 }
@@ -23,26 +71,66 @@ struct ContentView: View {
 extension ContentView {
     class ViewModel: ObservableObject {
         @Published var cities: [City] = []
+        @Published var isLoading = false
+        @Published var error: Error?
+        @Published var hasError = false
 
         var cancellable: AnyCancellable?
 
         func fetch() {
-            let url = URL(string: "https://api.openweathermap.org/data/2.5/group?units=metric&id=6295494,785842,2950158,2761369,292223,1880252,292968,792578,863883,3042030,2772400,2867714&appid=d37a967cdb5e2524a297ea5a2339efd0")!
+            let ids = [
+                "756135", // Warsaw
+                "683506", // Bucharest
+                "2657896", // Zürich
+                "2643743", // London
+                "2950159" // Berlin
+            ]
 
-            cancellable = URLSession.shared
-                .dataTaskPublisher(for: url)
+            let url = WeatherApi.group(ids: ids)
+            isLoading = true
+            hasError = false
+
+            cancellable = URLSession.shared.dataTaskPublisher(for: url)
                 .map(\.data)
                 .decode(type: CitiesResponse.self, decoder: JSONDecoder())
                 .map(\.list)
                 .receive(on: DispatchQueue.main)
-                .replaceError(with: [])
-                .assign(to: \.cities, on: self)
+                .sink(receiveCompletion: { [self] completion in
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        isLoading = false
+                    }
+                    if case let .failure(error) = completion {
+                        self.error = error
+                        self.hasError = true
+                    }
+                }, receiveValue: { cities in
+                    self.cities = cities
+                })
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+//        ContentView()
+
+//        ContentView()
+//            .environment(\.colorScheme, .dark)
+
+        LoadingOverlay(isLoading: true)
+    }
+}
+
+extension Weather {
+    var image: Image {
+        switch id {
+        case 200 ..< 300: return Image(systemName: "cloud.bolt.rain.fill")
+        case 300 ..< 400: return Image(systemName: "cloud.drizzle.fill")
+        case 500 ..< 600: return Image(systemName: "cloud.rain.fill")
+        case 600 ..< 700: return Image(systemName: "cloud.snow.fill")
+        case 700 ..< 800: return Image(systemName: "cloud.fog.fill")
+        case 800: return Image(systemName: "sun.max.fill")
+        default: return Image(systemName: "cloud.fill")
+        }
     }
 }
